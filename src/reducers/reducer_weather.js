13ef944b396cb0ade6f46d090abefdc7ec0fcfe4
Preethiba <ibpreethiba99@gmail.com>
2021-05-17 11:04:14 +0530

    1 file modified

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,7 @@
 export default function(state = null, action) {
-  console.log(action);
+  switch (action.type) {
+    case 'FETCH_WEATHER':
+      return [...state, action.payload];
+  }
   return state;
 }
