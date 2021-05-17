cd41f41eb249c9ff0d47977e81fc04a6698de272
Preethiba <ibpreethiba99@gmail.com>
2021-05-17 10:28:09 +0530

    1 file modified

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
 const API_KEY = '7fe6a7710c61d16d39d6129fae28a516';
+
+const ROOT_URL = 'http://';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const URL = `${ROOT_URL}&q=${city},us`;
+  const response = axios.get(URL);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: response
+  };
+}
