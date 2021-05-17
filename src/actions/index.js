39583a82a02fb4728bac250a3289711a731e8ea4
Preethiba <ibpreethiba99@gmail.com>
2021-05-17 10:30:45 +0530

    1 file modified

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const API_KEY = '7fe6a7710c61d16d39d6129fae28a516';
 
-const ROOT_URL = 'http://';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
