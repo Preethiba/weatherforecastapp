01b7c28461cf2dcc56cf3bd2daa8083a6465f395
Preethiba <ibpreethiba99@gmail.com>
2021-05-18 11:46:45 +0530

    1 file modified

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render() {
     return (
       <table class="table table-hover">
         <thead>
           <th>City</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Humidity</th>
         </thead>
+        <tbody>
+          <tr />
+        </tbody>
       </table>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    weather: state.weather
+  };
+}
+
+export default connect(mapStateToProps)(WeatherList);
