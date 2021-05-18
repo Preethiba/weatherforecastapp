8884dbc40eaafde997d63c23a6222a2137fd0b86
Preethiba <ibpreethiba99@gmail.com>
2021-05-18 11:40:55 +0530

    1 file modified

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,9 @@
-export default function(state = null, action) {
+import FETCH_WEATHER from '../actions';
+
+export default function(state = [], action) {
   switch (action.type) {
-    case 'FETCH_WEATHER':
-      return [...state, action.payload];
+    case FETCH_WEATHER:
+      return state.concat([action.payload.data]); //will not mutate state creates a new array similar to [action.payload.data, ...state]
   }
   return state;
 }
