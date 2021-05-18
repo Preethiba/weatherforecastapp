98eac5984ab8433a2c9e2fb8c954d26601fb0f05
Preethiba <ibpreethiba99@gmail.com>
2021-05-18 11:43:08 +0530

    1 file modified, 1 added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../style.css';
 import SearchBar from '../containers/SearchBar';
+import WeatherList from '../containers/WeatherList';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
---
+++ src/containers/WeatherList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class WeatherList extends Component {
+  render() {
+    return (
+      <table class="table table-hover">
+        <thead>
+          <th>City</th>
+        </thead>
+      </table>
+    );
+  }
+}
