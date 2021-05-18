63c7d2d8b9dacde90503a207cb1f43c94164a8d4
Preethiba <ibpreethiba99@gmail.com>
2021-05-18 12:32:27 +0530

    2 files modified, 1 added

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Charts from '../components/Charts';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    console.log(cityData);
     const name = cityData.city.name;
-    const temps = cityData.city.list.map(weather => weather.main.temp);
-    const humidity = cityData.city.list.map(weather => weather.main.humidity);
-    const pressure = cityData.city.list.map(weather => weather.main.pressure);
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
     return (
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Sparklines data={temps} height={120} width={180}>
-            <SparklinesLine color="red" />
-          </Sparklines>
-        </td>
-        {/* <td>
-          <Sparklines data={humidity} height={120} width={180}>
-            <SparklinesLine color="blue" />
-          </Sparklines>
-        </td>
-        <td>
-          <Sparklines data={pressure} height={120} width={180}>
-            <SparklinesLine color="green" />
-          </Sparklines>
-        </td> */}
+        <Charts color="red" data={temps} />
+        <Charts color="blue" data={humidity} />
+        <Charts color="brown" data={pressure} />
       </tr>
     );
   }
@@ -36,8 +25,8 @@ class WeatherList extends Component {
           <tr>
             <th>City</th>
             <th>Temperature</th>
-            <th>Pressure</th>
             <th>Humidity</th>
+            <th>Pressure</th>
           </tr>
         </thead>
         <tbody>{this.props.weather.map(this.renderWeather)}</tbody>
